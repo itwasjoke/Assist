b3d86b526f6b2dfd1ee5ce59aa7f9fe2577576cd
Andrew Vasilev <67266633+itwasjoke@users.noreply.github.com>
2022-05-29 18:02:58 +0300

    Update studios-control.php

--- pages/studios-control.php
+++ pages/studios-control.php
@@ -16,8 +16,8 @@ if (exist_data('new_info')){
 $sql_studio="SELECT * FROM studios s where s.id=".$studio_id;
 $studio=dataFromDb($sql_studio);
 $s_data=array();
-foreach ($studio as $fuck) {
-    foreach ($fuck as $key => $value) {
+foreach ($studio as $fuc) {
+    foreach ($fuc as $key => $value) {
         $s_data[$key]=$value;
     }
 }
