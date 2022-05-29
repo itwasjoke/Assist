0b9a9cec42db875108b741932174bead460b039f
itwasjoke <67266633+itwasjoke@users.noreply.github.com>
2022-05-29 15:21:07 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include './pages/header.php'; ?>
+
+<h2>Главная</h2>
+<center>
+	<h3>Заказывайте фильмы</h3>
+	<h3>Студии и обученные специалисты</h3>
+	<h3>Множество услуг на ваш выбор</h3>
+	<form name='something' method="post" action="/pages/services.php" style="width: 30%; padding-bottom: 50px;">
+		<input type='submit' class='btn_order' name='smting' value='Перейти к услугам'>
+	</form>
+</center>
+<img src="/img/header.jpg" style="width: 100%;">
+</body>
+</html>
