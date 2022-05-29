4e3bfcd55f784d482c6b7eefece8acbb4102d777
itwasjoke <67266633+itwasjoke@users.noreply.github.com>
2022-05-29 15:32:42 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 header('Content-Type:text/html; charset=utf-8');
 $host='localhost';
-$user ='u99924i2_db';
-$password='hsBt3&9g';
-$db = 'u99924i2_db';
+$user ='';
+$password='';
+$db = '';
 $con=mysqli_connect($host, $user, $password, $db);
 if ($con) {
     // echo 'yes';
